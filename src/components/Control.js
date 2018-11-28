b1a3ea61166a6bff7cc8825753c288c4093fde31
jeems-master <ric8932@outlook.com>
2018-11-27 20:53:16 -0500

    first commit

---
+++ src/components/Control.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';;
+
+
+function createWarning(funcName) {
+  return () => console.warn(funcName = 'is not defined');
+}
+
+class Control extends Component {
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.onPlus}>+</button>
+        <button onClick={this.props.onSubtract}>-</button>
+        <button onClick={this.props.onRandomizeColor}>Randomize Color</button>
+      </div>
+    )
+  }
+}
+
+Control.propTypes = {
+  onPlus: PropTypes.func,
+  onSubtract: PropTypes.func,
+  onRandomizeColor: PropTypes.func
+};
+
+
+Control.defaultProps = {
+  onPlus: createWarning('onPlus'),
+  onSubtract: createWarning('onSubtract'),
+  onRandomizeColor: createWarning('onRandomizeColor')
+};
+export default Control;
